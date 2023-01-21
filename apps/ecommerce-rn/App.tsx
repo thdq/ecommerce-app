74427d85b6e3b884d0c0244bb5a308d347d3c86d
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-21 16:45:31 -0300

    feat: providers from app

--- apps/ecommerce-rn/App.tsx
+++ apps/ecommerce-rn/App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { Provider as JotaiProvider } from 'jotai'
+import { NavigationContainer } from '@react-navigation/native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <SafeAreaProvider>
+      <JotaiProvider>
+        <NavigationContainer>
+          <View style={styles.container}>
+            <Text>Hello World!</Text>
+            <StatusBar style='auto' />
+          </View>
+        </NavigationContainer>
+      </JotaiProvider>
+    </SafeAreaProvider>
   )
 }
 
