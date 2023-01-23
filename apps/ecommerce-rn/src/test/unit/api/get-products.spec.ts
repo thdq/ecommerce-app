c9c6d5899263589474e1776566ce3d346e147612
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-23 11:41:59 -0300

    test: ensure function throw an error if API fails

--- apps/ecommerce-rn/src/test/unit/api/get-products.spec.ts
+++ apps/ecommerce-rn/src/test/unit/api/get-products.spec.ts
@@ -6,10 +6,20 @@ import mockAxios from 'jest-mock-axios'
 describe('fetch products', () => {
   describe('when API call is successful', () => {
     it('should return product list', async () => {
-      mockAxios.request.mockResolvedValue(productListMock)
+      mockAxios.request.mockResolvedValueOnce(productListMock)
 
       const result = await getProducts({})
       expect(result).toEqual(productListMock)
+    })
+  })
+
+  describe('when API call fails', () => {
+    it('should throw an error', async () => {
+      const error = new Error('An error occurred while fetching the product list')
+      mockAxios.request.mockRejectedValueOnce(new Error())
+
+      const result = getProducts({})
+      await expect(result).rejects.toThrow(error)
     })
   })
 })
