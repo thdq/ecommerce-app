9ea9ff7064de0b6298f7a8a109d86bb8d0b85ba3
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-23 22:38:17 -0300

    feat: use flatlist to render product card

--- apps/ecommerce-rn/src/components/ProductList/index.tsx
+++ apps/ecommerce-rn/src/components/ProductList/index.tsx
@@ -1,30 +1,42 @@
 import { useGetProducts } from '../../hooks/use-get-products'
-import { Product } from '../../models/product'
+import { Product, ProductType } from '../../models/product'
 import { ProductCard } from '../ProductCard'
 import styled from 'styled-components/native'
+import { ListRenderItem, FlatList as FlatListNative } from 'react-native'
 
 export const ProductList = () => {
   const { filteredList, error, isLoading } = useGetProducts()
   const products = filteredList?.products ?? []
 
+  const renderItem: ListRenderItem<ProductType> = ({ item }) => (
+    <ProductCard product={new Product(item)} />
+  )
+
   return (
     <ProductListContainer>
       {products.length ? (
-        products.map((product) => {
-          return (
-            <ProductCard testID='product-card' key={product.id} product={new Product(product)} />
-          )
-        })
+        <FlatList
+          data={products}
+          numColumns={2}
+          keyExtractor={(product: ProductType) => product.id.toString()}
+          renderItem={renderItem}
+        />
       ) : error ? (
         <Text testID='error'>Ocorreu um erro ao carregar a lista</Text>
       ) : isLoading ? (
         <Text>Carregando</Text>
-      ) : (
-        ''
-      )}
+      ) : null}
     </ProductListContainer>
   )
 }
 
-const ProductListContainer = styled.ScrollView``
+const ProductListContainer = styled.SafeAreaView`
+  flex: 1;
+`
+
+const FlatList = styled.FlatList`
+  flex: 1;
+  margin: 0 4px 0 4px;
+` as unknown as typeof FlatListNative
+
 const Text = styled.Text``
