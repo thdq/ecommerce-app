c9fd2a41c4f2bb4786b7146a7231502704724818
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-26 08:29:49 -0300

    chore: configure path alias in babel config

--- apps/ecommerce-rn/babel.config.js
+++ apps/ecommerce-rn/babel.config.js
@@ -2,6 +2,18 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@app': './src',
+          },
+        },
+      ],
+    ],
   }
 }
