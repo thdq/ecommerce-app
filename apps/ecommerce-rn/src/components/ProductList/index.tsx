30abf81e1738adbd1cd1b91eef54af750218cb1b
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-25 21:56:30 -0300

    feat: useModel hook

--- apps/ecommerce-rn/src/components/ProductList/index.tsx
+++ apps/ecommerce-rn/src/components/ProductList/index.tsx
@@ -1,17 +1,19 @@
 import { useGetProducts } from '../../hooks/use-get-products'
-import { ProductModel, ProductType } from '../../models/product'
+import { ProductType } from '../../models/product'
 import { ProductCard } from '../ProductCard'
 import styled from 'styled-components/native'
 import { ListRenderItem, FlatList as FlatListNative, RefreshControl } from 'react-native'
 import { ProductListError } from '../ProductListError'
 import { SkeletonCard } from 'ui-rn'
 import { useState, useCallback } from 'react'
+import { useProductModel } from '../../hooks/use-product-model'
 
 const NUMBER_COLUMNS = 2
 const SKELETON_FAKE_LIST = [...Array(6).keys()]
 
 export const ProductList = () => {
   const [isRefreshing, setRefreshing] = useState(false)
+  const { createModel } = useProductModel()
 
   const { filteredList, error, isLoading, mutate } = useGetProducts()
   const products = filteredList?.products ?? []
@@ -20,7 +22,7 @@ export const ProductList = () => {
 
   const renderSkeleton = () => <SkeletonCard />
   const renderItem: ListRenderItem<ProductType> = ({ item: product }) => (
-    <ProductCard product={new ProductModel(product)} />
+    <ProductCard product={createModel(product)} />
   )
 
   const onRefresh = useCallback(async () => {
