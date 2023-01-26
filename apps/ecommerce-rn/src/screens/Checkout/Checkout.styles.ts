9d535789ffc992d2be02d2b0eb22a9b80acfb652
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-26 08:55:23 -0300

    refactor: products screen folder

---
+++ apps/ecommerce-rn/src/screens/Checkout/Checkout.styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native'
+
+export const CheckoutContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-color: #f3f4f6;
+  padding: 0 32px;
+`
+export const TitleText = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 32px;
+  margin-bottom: 16px;
+`
--- apps/ecommerce-rn/src/screens/Checkout/Checkout.tsx
+++ apps/ecommerce-rn/src/screens/Checkout/Checkout.tsx
@@ -1,5 +1,5 @@
 import { ViewProps } from 'react-native'
-import styled from 'styled-components/native'
+import { CheckoutContainer, TitleText } from './Checkout.styles'
 import { Button } from 'ui-rn'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
@@ -16,18 +16,3 @@ export const Checkout = ({ navigation, ...props }: CheckoutProps) => {
     </CheckoutContainer>
   )
 }
-
-const CheckoutContainer = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  background-color: #f3f4f6;
-  padding: 0 32px;
-`
-const TitleText = styled.Text`
-  font-size: 18px;
-  font-weight: bold;
-  margin-top: 32px;
-  margin-bottom: 16px;
-`
