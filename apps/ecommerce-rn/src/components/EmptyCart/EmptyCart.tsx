eab4e3ee9b56dd9cde2b3e199849bbbbac755b81
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-26 08:33:58 -0300

    refactor: empty cart component folder

--- apps/ecommerce-rn/src/components/EmptyCart/EmptyCart.tsx
+++ apps/ecommerce-rn/src/components/EmptyCart/EmptyCart.tsx
@@ -1,5 +1,5 @@
+import { DescriptionText, EmptyCartContainer, TitleText } from './EmptyCart.styles'
 import { ViewProps } from 'react-native'
-import styled from 'styled-components/native'
 import { Button } from 'ui-rn'
 
 type EmptyCartProps = {
@@ -15,22 +15,3 @@ export const EmptyCart = ({ onShowProducts, ...props }: EmptyCartProps) => (
     <Button onPress={onShowProducts} label='Conferir produtos' />
   </EmptyCartContainer>
 )
-
-const EmptyCartContainer = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  background-color: #f3f4f6;
-  padding: 0 32px;
-`
-const TitleText = styled.Text`
-  font-size: 18px;
-  font-weight: bold;
-  margin-bottom: 8px;
-`
-
-const DescriptionText = styled.Text`
-  text-align: center;
-  margin-bottom: 32px;
-`
