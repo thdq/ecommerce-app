fcaa6c465b882472b7ab8fce6ceb3f3b47df9f6d
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-28 16:30:16 -0300

    perf: useCallback to render item and remove anon function

--- apps/ecommerce-rn/src/components/ProductList/ProductList.tsx
+++ apps/ecommerce-rn/src/components/ProductList/ProductList.tsx
@@ -3,6 +3,7 @@ import { ProductCard } from '@app/components/ProductCard'
 import { ListRenderItem, RefreshControl } from 'react-native'
 import { useProductModel } from '@app/hooks'
 import { FlatList, ProductListContainer } from './ProductList.styles'
+import { useCallback } from 'react'
 
 const NUMBER_COLUMNS = 2
 
@@ -15,16 +16,20 @@ type ProductListProps = {
 export const ProductList = ({ products, isRefreshing, onRefresh }: ProductListProps) => {
   const { createProductModel } = useProductModel()
 
-  const renderItem: ListRenderItem<Product> = ({ item: product }) => (
-    <ProductCard testID='product-card' product={createProductModel(product)} />
+  const renderItem: ListRenderItem<Product> = useCallback(
+    ({ item: product }) => (
+      <ProductCard testID='product-card' product={createProductModel(product)} />
+    ),
+    [],
   )
+  const getKeyExtractor = (product: Product) => product.id.toString()
 
   return (
     <ProductListContainer>
       <FlatList
         data={products}
         numColumns={NUMBER_COLUMNS}
-        keyExtractor={(product: Product) => product.id.toString()}
+        keyExtractor={getKeyExtractor}
         renderItem={renderItem}
         refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />}
       />
