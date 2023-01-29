d1b45b87ddb7524e8d5a408314ffdd6039f38f38
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-29 00:46:52 -0300

    fix: refresh product screen on focus back

--- apps/ecommerce-rn/src/screens/Products/Products.tsx
+++ apps/ecommerce-rn/src/screens/Products/Products.tsx
@@ -1,13 +1,26 @@
 import { useGetProducts } from '@app/hooks'
 import { ProductListError, ProductListLoading } from '@app/components'
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
+import { Text } from 'react-native'
 
 import { ProductList } from '@app/components'
+import { StackNavigationProps } from '@app/navigation/AppNavigation'
 
-const Products = () => {
+type CheckoutProps = {
+  navigation: StackNavigationProps
+}
+
+const Products = ({ navigation }: CheckoutProps) => {
   const [isRefreshing, setRefreshing] = useState(false)
   const { filteredList, error, isLoading, mutate } = useGetProducts()
   const products = filteredList?.products ?? []
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => onRefresh())
+    return () => {
+      unsubscribe
+    }
+  }, [navigation])
 
   const handleRetry = async () => await mutate()
 
