70e068504fc295856c589bead329231f019248ff
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-29 12:19:54 -0300

    feat: init i18n configuration with expo locale

---
+++ apps/ecommerce-rn/src/services/i18n.ts
@@ -0,0 +1,24 @@
+import ptBR from '@app/locales/pt-BR.json'
+import enUS from '@app/locales/en-US.json'
+import { locale } from 'expo-localization'
+
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+i18n.use(initReactI18next).init({
+  resources: {
+    'pt-BR': {
+      translation: ptBR,
+    },
+    'en-US': {
+      translation: enUS,
+    },
+  },
+  lng: locale,
+  fallbackLng: 'en-US',
+  interpolation: {
+    escapeValue: false,
+  },
+})
+
+export { i18n }
