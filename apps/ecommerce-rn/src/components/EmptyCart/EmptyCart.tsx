64bc3031cb6c3bfb066fad3b3ae6feff843f6481
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-29 13:37:17 -0300

    feat: i18n to EmptyCart component

--- apps/ecommerce-rn/src/components/EmptyCart/EmptyCart.tsx
+++ apps/ecommerce-rn/src/components/EmptyCart/EmptyCart.tsx
@@ -1,17 +1,20 @@
 import { DescriptionText, EmptyCartContainer, TitleText } from './EmptyCart.styles'
 import { ViewProps } from 'react-native'
 import { Button } from 'ui-rn'
+import { useTranslation } from 'react-i18next'
 
 type EmptyCartProps = {
   onShowProducts: () => void
 } & ViewProps
 
-export const EmptyCart = ({ onShowProducts, ...props }: EmptyCartProps) => (
-  <EmptyCartContainer {...props}>
-    <TitleText>Seu carrinho está vazio</TitleText>
-    <DescriptionText>
-      Comece adicionando um produto ao carrinho para realizar uma compra!
-    </DescriptionText>
-    <Button testID='show-products-button' onPress={onShowProducts} label='Conferir produtos' />
-  </EmptyCartContainer>
-)
+export const EmptyCart = ({ onShowProducts, ...props }: EmptyCartProps) => {
+  const { t } = useTranslation()
+
+  return (
+    <EmptyCartContainer {...props}>
+      <TitleText>{t('components.empty_cart.title')}</TitleText>
+      <DescriptionText>{t('components.empty_cart.description')}</DescriptionText>
+      <Button testID='show-products-button' onPress={onShowProducts} label={t('components.empty_cart.show_products') ?? ''} />
+    </EmptyCartContainer>
+  )
+}
