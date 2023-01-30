59a4b073ae35b81e8cdcba5434dd762202475cf0
Thiago Oliveira <thiagooliv_@outlook.com>
2023-01-30 01:04:13 -0300

    chore: init storybook dep

--- apps/ecommerce-rn/src/__tests__/setupTests.ts
+++ apps/ecommerce-rn/src/__tests__/setupTests.ts
@@ -1,3 +1,8 @@
 import '@testing-library/jest-native/extend-expect'
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
 jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'))
+
+jest.mock('global', () => ({
+  ...global,
+  WebSocket: function WebSocket() {},
+}))
